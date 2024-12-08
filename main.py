a6c46d3c17c70db670c63b96bb9291c38d4d31e7
Manish Makhija <makhijamanish386@gmail.com>
2024-12-08 20:14:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+a = 3
+b = 5
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a=b")
+else:
+    print("a is more than b")
+# wallet
+# if + for loop + while loop + function + class (optional
